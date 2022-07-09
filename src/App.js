5e0a1e8dbffcec8fcd5cfa0fbcb967fb00b1e382
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-09 22:13:31 +0530

    step 1.5.2.3 spliting components - multi level data passing using props

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 			amount: 94.12,
 			date: new Date(2020, 7, 14),
 		},
-		{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+		{ id: "e2", title: "New Toy TV", amount: 799.49, date: new Date(2021, 2, 12) },
 		{
 			id: "e3",
 			title: "Car Insurance",
-			amount: 294.67,
+			amount: 2094.67,
 			date: new Date(2021, 2, 28),
 		},
 		{
