d2bba171293841532e2c8f226fea021d25acd5e7
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-09 21:47:50 +0530

    step 1.5.1 patch -- js - converting date object to respective fields

--- src/components/expense/ExpenseItem.js
+++ src/components/expense/ExpenseItem.js
@@ -3,9 +3,17 @@ import React from "react";
 import "./ExpenseItem.scss"; // external library required
 
 const ExpenseItem = (props) => {
+	const day = props.date.toLocaleString("en-IN", { day: "2-digit" });
+	const month = props.date.toLocaleString("en-IN", { month: "long" });
+	const year = props.date.getFullYear();
+
 	return (
 		<div className="expense-item">
-			<div> {props.date.toISOString()} </div>
+			<div>
+				<div> {day} </div>
+				<div> {month} </div>
+				<div> {year} </div>
+			</div>
 			<div className="expense-item__description">
 				<h2> {props.title} </h2>
 				<div className="expense-item__price">${props.amount}</div>
