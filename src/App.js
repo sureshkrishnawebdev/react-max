6b9a8b03ed8af017cf6b5985e087fccd0326acb7
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-09 16:20:02 +0530

    step 1.2. created a static custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import ExpenseItem from './components/expense/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/expense/ExpenseItem.js
@@ -0,0 +1,16 @@
+
+
+const ExpenseItem = () => {
+
+    return (
+        <div className="expense-item">
+            <div> 07 July 2022 </div>
+            <div className="expense-item__description">
+                <h2> Udemy </h2>
+                <div className="expense-item__price"> $10 </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
