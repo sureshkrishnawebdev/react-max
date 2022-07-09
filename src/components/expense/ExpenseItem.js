4040bd7b61632247e649c593e584ff7ce7f27da1
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-09 22:17:42 +0530

    step 1.5.2.3 spliting components - multi level data passing using props

--- src/components/expense/ExpenseItem.js
+++ src/components/expense/ExpenseItem.js
@@ -1,18 +1,22 @@
 import React from "react";
 // import './ExpenseItem.css';     // no external library required
 import "./ExpenseItem.scss"; // external library required
-import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+	const day = props.date.toLocaleString("en-IN", { day: "2-digit" });
+	const month = props.date.toLocaleString("en-IN", { month: "long" });
+	const year = props.date.getFullYear();
+
 	return (
 		<div className="expense-item">
-			<ExpenseDate date={props.date} />
+			<div>
+				<div> {day} </div>
+				<div> {month} </div>
+				<div> {year} </div>
+			</div>
 			<div className="expense-item__description">
 				<h2> {props.title} </h2>
-				<div className="expense-item__price">
-					{" "}
-					<span>&#8377;</span> {props.amount}
-				</div>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
