17bd1030ec7d2151977ef9ac5f04fd13a7a6f96d
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-09 21:20:49 +0530

    1.5.1 pass data - props intro

--- src/components/expense/ExpenseItem.js
+++ src/components/expense/ExpenseItem.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React from "react";
 // import './ExpenseItem.css';     // no external library required
-import './ExpenseItem.scss';    // external library required
+import "./ExpenseItem.scss"; // external library required
 
-const ExpenseItem = () => {
-
-    const expenseDate = new Date(7, 7, 2022).toISOString();
-    const expenseTitle = 'React - The Complete Guide (incl Hooks, React Router, Redux)';
-    const expenseAmount = 450;
-
-    return (
-        <div className="expense-item">
-            <div> { expenseDate } </div>
-            <div className="expense-item__description">
-                <h2> { expenseTitle } </h2>
-                <div className="expense-item__price"> ${ expenseAmount } </div>
-            </div>
-        </div>
-    )
-}
+const ExpenseItem = (props) => {
+	return (
+		<div className="expense-item">
+			<div> {props.date.toISOString()} </div>
+			<div className="expense-item__description">
+				<h2> {props.title} </h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+		</div>
+	);
+};
 
 export default ExpenseItem;
