c3fa7f2b987394fe15f6c178a5f290c4ecf9fcb8
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-20 05:50:00 +0530

    router-step-1.3.1. Linking the route -- default href

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import MainHeader from './components/Layout/MainHeader';
 import ExpenseTracker from "./pages/ExpenseTracker";
 import FoodApp from "./pages/FoodApp";
 import Home from "./pages/Home";
@@ -7,15 +8,18 @@ import Home from "./pages/Home";
 function App() {
 	return (
 		<React.Fragment>
-			<Route path="/home">
-				<Home />
-			</Route>
-			<Route path="/expense-tracker">
-				<ExpenseTracker />
-			</Route>
-			<Route path="/food-app">
-				<FoodApp />
-			</Route>
+			<MainHeader />
+			<main>
+				<Route path="/home">
+					<Home />
+				</Route>
+				<Route path="/expense-tracker">
+					<ExpenseTracker />
+				</Route>
+				<Route path="/food-app">
+					<FoodApp />
+				</Route>
+			</main>
 		</React.Fragment>
 	);
 }
