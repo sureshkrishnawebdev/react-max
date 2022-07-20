2bbd268ad4329ad19be1546233c1b927414ca15b
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-20 18:15:27 +0530

    router-step-1.1. Defining the route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+
+// Components
+import Home from './pages/Home';
+import ExpenseTracker from './pages/ExpenseTracker';
+import FoodApp from './pages/FoodApp';
+
 function App() {
 	return (
-	  <div>
-		<h2>Let's get started!</h2>
-	  </div>
+	  <Fragment>
+		<main>
+			<Route path="/home">
+				<Home />
+			</Route>
+			<Route path="/expense-tracker">
+				<ExpenseTracker />
+			</Route>
+			<Route path="/food-app">
+				<FoodApp />
+			</Route>
+		</main>
+	  </Fragment>
 	);
   }
   
---
+++ src/pages/ExpenseTracker.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const ExpenseTracker = () => {
+    return (
+        <Fragment>
+            <h1> Expense Tracker </h1>
+            <p> Calculating your expenses... </p>
+        </Fragment>
+    )
+}
+
+export default ExpenseTracker;
