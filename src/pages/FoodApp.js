b487b87f9a2c36e465fb6529cdc9aa9f93cdfaa6
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-21 22:51:54 +0530

    step 2.3. Initial Meal component setup completed

--- src/pages/FoodApp.js
+++ src/pages/FoodApp.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from 'react';
 
-// Component - Layout
+// ------ Component ----------
+// Layout
 import Header from '../components/food-app/components/Layout/header/Header';
+// Feature
+import Meals from '../components/food-app/components/Meals/meals/Meals';
 
 const FoodApp = () => {
     return (
         <Fragment>
             <Header />
+            <main>
+                <Meals />
+            </main>
         </Fragment>
     )
 }
