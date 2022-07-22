eb61c632bd4527d03cc7f8be459046db607b74a9
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-07-22 08:42:46 +0530

    step 2.4. Added Modal(overlay) component - patch 2

---
+++ src/components/food-app/components/UI/modal/Modal.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+import { ReactDOM } from 'react-dom';
+
+// Styles
+import classes from './Modal.module.css';
+
+// Components
+
+const Backdrop = (props) => {
+    return (
+        <div className={classes.backdrop}>{props.children}</div>
+    )
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+	return (
+        <Fragment>
+            {ReactDOM.createPortal( <Backdrop />,  portalElement)}
+            {ReactDOM.createPortal( <ModalOverlay> {props.children} </ModalOverlay>, portalElement)}
+        </Fragment>
+	)
+};
+
+export default Modal;
